83f77db0c63a17daebfea82846b18164a965e5f4
JM0320 <zxij@hanmail.net>
2015-05-19 20:03:16 +0900

    finat_project1

---
+++ Release/ssp.d
@@ -0,0 +1,57 @@
+ssp.d ssp.o: ../ssp.cpp ../ssp.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Arduino.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/binary.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/WCharacter.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/WString.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/HardwareSerial.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Stream.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Print.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Printable.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/new.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\variants\mega/pins_arduino.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Wire.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/I2Cdev.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Arduino.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Wire.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/MPU6050_6Axis_MotionApps20.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/I2Cdev.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/helper_3dmath.h \
+  C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/MPU6050.h
+
+../ssp.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Arduino.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/binary.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/WCharacter.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/WString.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/HardwareSerial.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Stream.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Print.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Printable.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/new.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\variants\mega/pins_arduino.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Wire.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/I2Cdev.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Arduino.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/Wire.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/MPU6050_6Axis_MotionApps20.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/I2Cdev.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/helper_3dmath.h:
+
+C:\AndroXStudio\legacy\arduino\hardware\arduino\cores\arduino/MPU6050.h:
